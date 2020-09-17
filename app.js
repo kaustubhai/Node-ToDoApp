48414bcfcabb3f6622e814b873b82cfb55089519
kaustubh <kaustubh229@gmail.com>
2020-09-17 11:16:41 +0530

    Handlers added

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-const get = require('./notes');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-let p = './notes.txt';
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log(chalk.greenBright.inverse(' Added '))
+    }
+})
+    
+yargs.command({
+    command: 'delete',
+    describe: 'Delete a note',
+    handler: function () {
+        console.log(chalk.red.inverse(' Deleted '))
+    }
+})
 
-get(p);
+yargs.command({
+    command: 'list',
+    describe: 'list all the todos',
+    handler: function () {
+        console.log(chalk.yellowBright.inverse(' Listed '))
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a todo task',
+    handler: function () {
+        console.log(chalk.green.inverse(' Read '))
+    }
+})
+console.log(yargs.argv)
--- notes.js
+++ notes.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 const chalk = require('chalk');
-const { inverse } = require('chalk');
-const msg = chalk.bold.blue
+const yargs = require('yargs');
 
-const getNote = (p) => {
-    fs.readFile(p, (err, fileContent) => {
-        if (!err)
-            console.log(msg(fileContent));
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Added')
+    }
+    },
+    {
+        command: 'delete',
+        describe: 'Delete a note',
+        handler: function () {
+            console.log.red('Deleted')
+        }
     })
-}
-
-module.exports = getNote;
